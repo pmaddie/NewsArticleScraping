6adb194385ed92c83cd0838e840250757ebd8283
Maddie P <madelineprusak@gmail.com>
2019-04-21 15:43:39 -0400

    5 required packages installed, files and folders set up.

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var logger = require("express-handlebars");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+//Initialize express
+var app = express();
+
+//Routes
+
