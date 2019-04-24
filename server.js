b8abb10ce384cbfd57090229435891c5a9f3e7bf
Maddie P <madelineprusak@gmail.com>
2019-04-23 23:00:58 -0400

    handlebars set up

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //dependencies
 var express = require("express");
+ //Initialize express
+var app = express();
 var mongoose = require("mongoose");
 
 // Our scraping tools
@@ -8,42 +10,59 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+
+// Make public a static folder
+app.use(express.static("public"));
+
+
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Require all models
-var db = require("./models");
-
-var PORT = 3000;
-
-//Initialize express
-var app = express();
-// Configure middleware
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
 
 
-
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
-
-//Routes
-//route code here
-
+mongoose.connect("mongodb://localhost/mongoHeadlines");
 // check connection status
-let dB = mongoose.connection;
-dB.on('error', (error)=>{
+let db = mongoose.connection;
+db.on('error', (error)=>{
     console.log(`Connection error ${error}`);
 });
 
+
+
+
+var PORT = 3000;
 // start server
 app.listen(PORT, ()=>{
     console.log(`App running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+// // Require all models
+// var db = require("./models");
+
+// // Configure middleware
+// // Parse request body as JSON
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+
+
+
+// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+
+
+// //Routes
+// //route code here
+
